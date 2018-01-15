31d06dbe873d26a01f04f17d2cab62591c243744
Chaim Greenberg <chaim8greenberg@gmail.com>
2018-01-15 13:21:24 +0200

    version 15

--- src/app/datasource.model.ts
+++ src/app/datasource.model.ts
@@ -6,7 +6,7 @@ export class SimpleDataSource {
     constructor() {
         this.data = new Array<Product>(
         new Product(1, "TANAH", "Books", 275),
-        new Product(2, "TEHILIM", "Books", 70.95),
+        new Product(2, "TEHILIM", "Books", 5),
         new Product(3, "Kidush Cup", "For Shabat", 199.50),
         new Product(4, "Nerot shel shabat", "For Shabat", 34.95),
         new Product(5, "Talit Gadol", "Talits", 299));
